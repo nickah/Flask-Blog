8f7d698ef160a6df39a48fa760c1c62c0d971239
Nick <nick@nick-lappy.(none)>
2012-03-28 15:22:01 +0100

    Starting off, got a list of posts to show and an attempt at post/id

---
+++ blog.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+titles = ["First post", "Second Post"]
+
+posts = ["hello this is a test post", "hello this is my second post"]
+
+# /post/1/
+@app.route("/post/<n>")
+def show_post(n):
+    try:
+        post = ""
+        post += titles[int(n) - 1] + "<br />\n" + "_________" + "<br />\n" +  posts[int(n) - 1]
+        return post
+    except IndexError:        
+	return "post does not exist!"
+
+@app.route("/")
+def list_posts():
+    html = ""
+    for title in titles:
+        html += title + "poo" +  "<br />\n"
+    return html
+
+app.run(debug=True)
+
+
+    
